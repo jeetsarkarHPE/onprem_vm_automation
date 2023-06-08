48292599103eac977549d4def324fad92e4305f1
sachinthra-nv <sachinthra-n-v@hpe.com>
2023-06-08 12:05:21 +0530

    new

---
+++ temp.sh
@@ -0,0 +1,8 @@
+python3 deploy_ope_vm.py
+        -oU "http://16.182.31.122:9000/golden-image/glcp-onprem-combined-1.0.0-10.ova"
+        -oH "op360-g10s04-vm01.hstlabs.glcp.hpecorp.net"
+        -vcenter "m2-dl380g10-74-vm01.mip.storage.hpecorp.net"
+        -u "administrator@vsphere.local"
+        -p "Nim123Boli#"
+        -ds "datastore_1_3"
+        -dc "cluster-5"
